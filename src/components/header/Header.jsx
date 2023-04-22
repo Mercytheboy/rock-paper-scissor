59f49b4f164633de39ea4a27a15572c4a7ec7008
Mercy Dare <daremercy1@gmail.com>
2023-04-22 09:59:26 +0100

    header styling design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import "./header.css";
+
 function Header({ score }) {
-  return <div>score: {score}</div>;
+  return (
+    <div className="header">
+      <div className="header__title">
+        <p>Rock</p>
+        <p>Paper</p>
+        <p>Scissors</p>
+      </div>
+      <div className="header__score-box">
+        <p>score</p>
+        <p className="header__score">{score}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
